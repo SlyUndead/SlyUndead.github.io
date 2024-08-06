68221aac00c2d9e4a26d2b3e95e57242fc9180ad
SlyUndead <128504577+SlyUndead@users.noreply.github.com>
2024-08-07 00:50:23 +0530

    Delete dental1 directory

--- dental1/src/customImageLoader.js
+++
@@ -1,9 +0,0 @@
-import * as cornerstone from '@cornerstonejs/core';
-import cornerstoneWebImageLoader from '@cornerstonejs/web-image-loader';
-
-// Register the custom image loader
-cornerstone.registerImageLoader('base64', cornerstoneWebImageLoader.loadImage);
-
-export default function loadBase64Image(imageId) {
-  return cornerstone.loadImage(imageId);
-}
